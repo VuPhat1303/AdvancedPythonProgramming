e1ac3a99be663c72e694dc1afe4a3fe78a10bc19
Your Name <you@example.com>
2021-11-09 22:53:31 +0700

    update

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def trangchu():
+    return render_template("index.html")
+
+@app.route("/tintuc.html")
+def tintuc():
+    return render_template("tintuc.html")
+
+@app.route("/sanpham.html")
+def sanpham():
+    return render_template("sanpham.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
